28a2b30555c8dbe1ffb6d2afa8d81d0372f822b7
Dilshan Liyanage <48806985+dilshanliyanage1000@users.noreply.github.com>
2023-07-22 18:13:42 -0400

    Added category backend

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,8 @@ app.get("/", (req, res) => {
 require("./app/routes/product.routes")(app);
 require("./app/routes/user.routes")(app);
 require("./app/routes/order.routes")(app);
+require("./app/routes/category.routes")(app);
+
 // set port, listen for requests
 
 const PORT = process.env.PORT || 3000;
