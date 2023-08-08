19756ed4201d976e9eac16b4ef9c3134ef313699
Dilshan Liyanage <48806985+dilshanliyanage1000@users.noreply.github.com>
2023-08-07 20:19:37 -0400

    done!

--- backend/app/config/auth.config.js
+++ backend/app/config/auth.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  
-  secret: "grocery-store-key",
-  jwtExpiration: 3600,
-  jwtRefreshExpiration: 86400,
-  
-  jwtExpiration: 60
 
+  secret: "grocery-store-key",
+
+  jwtExpiration: 3600,
+
+  jwtRefreshExpiration: 86400
+  
 };
